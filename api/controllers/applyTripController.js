23f96af17e4e06a98fbab54381d245410da2b64c
Julia Palacios <moreparaya@gmail.com>
2021-04-10 23:21:04 +0000

    auth control

--- api/controllers/applyTripController.js
+++ api/controllers/applyTripController.js
@@ -2,157 +2,206 @@
 
 
 var mongoose = require('mongoose'),
-  ApplyTrip = mongoose.model('ApplyTrips');
+  ApplyTrip = mongoose.model('ApplyTrips'),
+  Trip = mongoose.model('Trips'),
+  Actor = mongoose.model('Actors');
 
-exports.list_all_applications = function(req, res) {
-  ApplyTrip.find({}, function(err, application) {
-    if (err){
+exports.list_all_applications = function (req, res) {
+  ApplyTrip.find({}, function (err, application) {
+    if (err) {
       res.status(500).send(err);
     }
-    else{
+    else {
       res.json(application);
     }
   });
 };
 
-exports.list_my_applications = function(req, res) {
-  ApplyTrip.find(function(err, applications) {
-    if (err){
+exports.list_my_applications = function (req, res) {
+  ApplyTrip.find(function (err, applications) {
+    if (err) {
       res.status(500).send(err);
     }
-    else{
+    else {
       res.json(applications);
     }
   });
 };
 
 
-exports.create_application = function(req, res) {
+exports.create_application = function (req, res) {
   var new_appl = new ApplyTrip(req.body);
+  var idToken = req.headers['idtoken'];
 
-  new_appl.save(function(error, application) {
-    if (error){
-      if(err.name=='ValidationError') {
-          res.status(422).send(err);
-        }
-        else{
-          res.status(500).send(err);
+  admin.auth().verifyIdToken(idToken).then(function (decodedToken) {
+
+    var uid = decodedToken.uid;
+
+    Actor.findOne({ email: uid }, function (err, actor) {
+      if (err) {
+        res.send(err);
+      } else {
+        if (req.body.trip) {
+          Trip.findById(req.body.trip, function (err, trip) {
+            if (err) {
+              //res.status(500).send(err);
+              res.status(403);
+              res.json("Application cannot be created. Trip does not exist!");
+
+            } else {
+              if (trip.status.includes('PUBLISHED')) {
+                new_appl.explorer = actor;
+                new_appl.save(function (err, appl) {
+                  if (err) {
+                    console.log(new_appl)
+                    console.log(Date() + " - " + err);
+                    if (err.name == 'ValidationError') {
+                      res.status(422).send(err);
+                    }
+                    else {
+                      res.status(500).send(err);
+                    }
+                  }
+                  else {
+                    res.json(appl);
+                  }
+                });
+              }
+            }
+          });
         }
       }
-    else{
-      res.json(application);
-    }
+    });
   });
 };
 
-exports.search_application = function(req, res) {
+exports.search_application = function (req, res) {
   var query = {};
   //if managerId is null, i.e. parameter is not in the URL, the search retrieves applications not assined to any manager
   //else, the search retrieves applications assined to the specified application
   query.manager = req.query.managerId;
 
-  if (req.query.cancelled=="true") {
+  if (req.query.cancelled == "true") {
     //retrieving applications with a statusUpdateMoment
     query.statusUpdateMoment = { $exists: true }
   }
-  if (req.query.cancelled=="false") {
+  if (req.query.cancelled == "false") {
     //retrieving applications without a statusUpdateMoment
     query.statusUpdateMoment = { $exists: false };
   }
-  if (req.query.accepted=="true") {
+  if (req.query.accepted == "true") {
     //retrieving applications with a statusUpdateMoment
     query.acceptanceMoment = { $exists: true }
   }
-  if (req.query.accepted=="false") {
+  if (req.query.accepted == "false") {
     //retrieving applications without a statusUpdateMoment
     query.acceptanceMoment = { $exists: false };
   }
 
-  var skip=0;
-  if(req.query.startFrom){
+  var skip = 0;
+  if (req.query.startFrom) {
     skip = parseInt(req.query.startFrom);
   }
-  var limit=0;
-  if(req.query.pageSize){
-    limit=parseInt(req.query.pageSize);
+  var limit = 0;
+  if (req.query.pageSize) {
+    limit = parseInt(req.query.pageSize);
   }
 
-  var sort="";
-  if(req.query.reverse=="true"){
-    sort="-";
+  var sort = "";
+  if (req.query.reverse == "true") {
+    sort = "-";
   }
-  if(req.query.sortedBy){
-    sort+=req.query.sortedBy;
+  if (req.query.sortedBy) {
+    sort += req.query.sortedBy;
   }
 
-  console.log("Query: "+query+" Skip:" + skip+" Limit:" + limit+" Sort:" + sort);
+  console.log("Query: " + query + " Skip:" + skip + " Limit:" + limit + " Sort:" + sort);
 
   ApplyTrip.find(query)
-       .sort(sort)
-       .skip(skip)
-       .limit(limit)
-       .lean()
-       .exec(function(err, application){
-    console.log('Start searching applications');
-    if (err){
-      res.send(err);
-    }
-    else{
-      res.json(application);
-    }
-    console.log('End searching applications');
-  });
+    .sort(sort)
+    .skip(skip)
+    .limit(limit)
+    .lean()
+    .exec(function (err, application) {
+      console.log('Start searching applications');
+      if (err) {
+        res.send(err);
+      }
+      else {
+        res.json(application);
+      }
+      console.log('End searching applications');
+    });
 };
 
-exports.read_application = function(req, res) {
-  ApplyTrip.findById(req.params.applicationId, function(err, application) {
-    if (err){
+exports.read_application = function (req, res) {
+  ApplyTrip.findById(req.params.applicationId, function (err, application) {
+    if (err) {
       res.status(500).send(err);
     }
-    else{
+    else {
       res.json(application);
     }
   });
 };
 
 
-exports.update_application = function(req, res) {
+exports.update_application = function (req, res) {
   //Check if the application has been previously assigned or not
   //Assign the application to the proper manager that is requesting the assigment
   //when updating delivery moment it must be checked the manager assignment and to check if it is the proper manager and if not: res.status(403); "an access token is valid, but requires more privileges"
-  ApplyTrip.findById(req.params.applicationId, function(err, application) {
-    if (err){
-      if(err.name=='ValidationError') {
-          res.status(422).send(err);
-      }
-      else{
-        res.status(500).send(err);
-      }
-    }
-    else{
-        ApplyTrip.findOneAndUpdate({_id: req.params.applicationId}, req.body, {new: true}, function(err, application) {
-          if (err){
-            res.status(500).send(err);
+  var idToken = req.headers['idtoken'];
+
+  admin.auth().verifyIdToken(idToken).then(function (decodedToken) {
+
+    var uid = decodedToken.uid;
+
+    Actor.findOne({ email: uid }, function (err, actor) {
+      if (err) {
+        res.send(err);
+      } else {
+
+        ApplyTrip.findById(req.params.applicationId, function (err, application) {
+          if (err) {
+            if (err.name == 'ValidationError') {
+              res.status(422).send(err);
+            }
+            else {
+              res.status(500).send(err);
+            }
           }
-          else{
-            res.json(application);
+          else {
+            if (String(actor._id) == String(application.explorer)) {
+              ApplyTrip.findOneAndUpdate({ _id: req.params.applicationId }, req.body, { new: true }, function (err, application) {
+                if (err) {
+                  res.status(500).send(err);
+                }
+                else {
+                  res.json(application);
+                }
+              });
+            } else {
+              res.status(403);
+              res.json("You cannot edit others explorers applications!")
+            }
           }
         });
       }
+    });
   });
 };
 
 
-exports.delete_application = function(req, res) {
+exports.delete_application = function (req, res) {
   //Check if the application were delivered or not and delete it or not accordingly
   //Check if the user is the proper customer that posted the application and if not: res.status(403); "an access token is valid, but requires more privileges"
   ApplyTrip.deleteOne({
     _id: req.params.orderId
-  }, function(err, application) {
-    if (err){
+  }, function (err, application) {
+    if (err) {
       res.status(500).send(err);
     }
-    else{
+    else {
       res.json({ message: 'ApplyTrip successfully deleted' });
     }
   });
